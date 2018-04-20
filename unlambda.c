0127d88950cb7b4bbd2ea5125eafc0136d035967
irori <irorin@gmail.com>
2018-04-20 00:41:40 -0700

    Initial commit

---
+++ unlambda.c
@@ -0,0 +1,377 @@
+#include <assert.h>
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#define INITIAL_HEAP_SIZE 512*1024
+#define GC_MARGIN 10
+
+typedef enum {
+  // Expressions
+  AP, I, DOT, K1, K, S2, S1, S, V, D1, D, CONT, C, E, AT, QUES, PIPE,
+  // Continuations
+  EVAL,
+  APP1,
+  APP,
+  DEL,
+  FINAL,
+  // GC
+  COPIED,
+} CellType;
+
+typedef struct _Cell {
+  CellType t;
+  struct _Cell *l, *r;
+} Cell;
+
+Cell constI = {I};
+Cell constK = {K};
+Cell constS = {S};
+Cell constV = {V};
+Cell constD = {D};
+Cell constC = {C};
+Cell constE = {E};
+Cell constAt = {AT};
+Cell constPipe = {PIPE};
+
+Cell *heap_area, *free_ptr;
+int heap_size, next_heap_size;;
+
+int gc_notify = 0;
+double total_gc_time = 0.0;
+
+int current_ch = EOF;
+Cell* cont;
+#define PUSHCONT(t, v) (cont = new_cell(t, cont, v))
+#define POPCONT (cont = cont->l)
+
+void errexit(char *fmt, ...) {
+    va_list arg;
+    va_start(arg, fmt);
+    vfprintf(stderr, fmt, arg);
+    va_end(arg);
+
+    exit(1);
+}
+
+// Storage -------------------------
+
+void storage_init(int size) {
+    heap_size = size;
+    heap_area = malloc(sizeof(Cell) * heap_size);
+    if (heap_area == NULL)
+	errexit("Cannot allocate heap storage (%d cells)\n", heap_size);
+    
+    free_ptr = heap_area;
+    heap_area += heap_size;
+    next_heap_size = heap_size * 3 / 2;  
+}
+
+Cell* new_cell(CellType t, Cell* l, Cell* r) {
+  if (free_ptr >= heap_area)
+    errexit("Out of memory\n");
+  Cell* c = free_ptr++;
+  c->t = t;
+  c->l = l;
+  c->r = r;
+  return c;
+}
+
+Cell* copy_cell(Cell* c)
+{
+  switch (c->t) {
+  case COPIED:
+    return c->l;
+  case I: case K: case S: case V: case D: case C: case E: case AT: case PIPE:
+    return c;
+  default:
+    break;
+  }
+
+  Cell* r = free_ptr++;
+  r->t = c->t;
+  r->l = c->l;
+  r->r = c->r;
+  c->t = COPIED;
+  c->l = r;
+  return r;
+}
+
+Cell* gc_run(Cell* save) {
+  Cell* free_area = NULL;
+  int num_alive;
+  Cell* scan;
+  clock_t start = clock();
+
+  if (free_area == NULL) {
+    free_area = malloc(sizeof(Cell) * next_heap_size);
+    if (free_area == NULL)
+      errexit("Cannot allocate heap storage (%d cells)\n",
+	      next_heap_size);
+  }
+
+  free_ptr = scan = free_area;
+  free_area = heap_area - heap_size;
+  heap_area = free_ptr + next_heap_size;
+
+  if (save)
+    save = copy_cell(save);
+  if (cont)
+    cont = copy_cell(cont);
+
+  while (scan < free_ptr) {
+    switch (scan->t) {
+    case COPIED:
+      errexit("[BUG] cannot happend\n");
+      break;
+    case K1:
+    case S1:
+    case D1:
+    case CONT:
+    case EVAL:
+      scan->l = copy_cell(scan->l);
+      break;
+    case AP:
+    case S2:
+    case APP1:
+    case APP:
+    case DEL:
+      scan->l = copy_cell(scan->l);
+      scan->r = copy_cell(scan->r);
+      break;
+    default:
+      break;
+    }
+    scan++;
+  }
+
+  num_alive = free_ptr - (heap_area - next_heap_size);
+  if (gc_notify)
+    fprintf(stderr, "GC: %d / %d\n", num_alive, heap_size);
+
+  if (heap_size != next_heap_size || num_alive * 8 > next_heap_size) {
+    heap_size = next_heap_size;
+    if (num_alive * 8 > next_heap_size)
+      next_heap_size = num_alive * 8;
+
+    free(free_area);
+    free_area = NULL;
+  }
+
+  total_gc_time += (clock() - start) / (double)CLOCKS_PER_SEC;
+  return save;
+}
+
+// Parser -------------------------
+
+Cell* parse(FILE* fp) {
+  int ch;
+  do {
+    ch = fgetc(fp);
+    if (ch == '#') {
+      while (ch = fgetc(fp), ch != '\n' && ch != EOF)
+	;
+    }
+  } while (isspace(ch));
+  switch (ch) {
+  case '`':
+    {
+      Cell* l = parse(fp);
+      Cell* r = parse(fp);
+      return new_cell(AP, l, r);
+    }
+  case 'i': case 'I': return &constI;
+  case 'k': case 'K': return &constK;
+  case 's': case 'S': return &constS;
+  case 'v': case 'V': return &constV;
+  case 'd': case 'D': return &constD;
+  case 'c': case 'C': return &constC;
+  case 'e': case 'E': return &constE;
+  case 'r': case 'R': return new_cell(DOT, (Cell*)'\n', NULL);
+  case '@': return &constAt;
+  case '|': return &constPipe;
+  case '.': case '?':
+    {
+      int ch2 = fgetc(fp);
+      if (ch2 == EOF)
+	errexit("unexpected EOF\n");
+      return new_cell(ch == '.' ? DOT : QUES, (Cell*)ch2, NULL);
+    }
+  case EOF:
+    errexit("unexpected EOF\n");
+  }
+  errexit("unexpected character %c\n", ch);
+  return NULL;
+}
+
+Cell* load_program(const char* fname) {
+    FILE* fp;
+    Cell* c;
+
+    if (fname == NULL)
+	fp = stdin;
+    else {
+	fp = fopen(fname, "r");
+	if (fp == NULL)
+	    errexit("cannot open %s\n", fname);
+    }
+
+    c = parse(fp);
+
+    if (fname != NULL)
+	fclose(fp);
+
+    return c;
+}
+
+// Evaluator
+
+Cell* eval(Cell* e) {
+  while (e->t == AP) {
+    if (free_ptr >= heap_area)
+      e = gc_run(e);
+    PUSHCONT(APP1, e->r);
+    e = e->l;
+  }
+  return e;
+}
+
+Cell* apply(Cell* rator, Cell* rand) {
+  switch (rator->t) {
+  case I:
+    return rand;
+  case DOT:
+    putchar((int)rator->l);
+    return rand;
+  case K1:
+    return rator->l;
+  case K:
+    return new_cell(K1, rand, NULL);
+  case S2:
+    {
+      Cell* e2 = new_cell(AP, rator->r, rand);
+      PUSHCONT(APP1, e2);
+      PUSHCONT(APP1, rand);
+      return rator->l;
+    }
+  case S1:
+    return new_cell(S2, rator->l, rand);
+  case S:
+    return new_cell(S1, rand, NULL);
+  case V:
+    return rator;
+  case D1:
+    PUSHCONT(DEL, rand);
+    PUSHCONT(EVAL, NULL);
+    return rator->l;
+  case D:
+    return new_cell(D1, rand, NULL);
+  case CONT:
+    cont = rator->l;
+    return rand;
+  case C:
+    {
+      Cell* val = new_cell(CONT, cont, NULL);
+      PUSHCONT(APP, rand);
+      return val;
+    }
+  case E:
+    cont = new_cell(FINAL, NULL, NULL);
+    return rand;
+  case AT:
+    current_ch = getchar();
+    PUSHCONT(APP, rand);
+    return current_ch == EOF ? &constV : &constI;
+  case QUES:
+    PUSHCONT(APP, rand);
+    return current_ch == (int)rator->l ? &constI : &constV;
+  case PIPE:
+    PUSHCONT(APP, rand);
+    return current_ch == EOF ? &constV : new_cell(DOT, (Cell*)current_ch, NULL);
+  default:
+    errexit("[BUG] apply: invalid operator type %d\n", rator->t);
+  }
+  return NULL;
+}
+
+void run(Cell* val) {
+  PUSHCONT(FINAL, NULL);
+  PUSHCONT(EVAL, NULL);
+  for (;;) {
+    if (free_ptr + GC_MARGIN >= heap_area)
+      val = gc_run(val);
+
+    switch (cont->t) {
+    case EVAL:
+      POPCONT;
+      val = eval(val);
+      break;
+    case APP1:
+      if (val->t == D) {
+	val = new_cell(D1, cont->r, NULL);
+	POPCONT;
+      } else {
+	Cell* rand = cont->r;
+	POPCONT;
+	PUSHCONT(APP, val);
+	val = eval(rand);
+      }
+      break;
+    case APP:
+      {
+	Cell* rator = cont->r;
+	POPCONT;
+	val = apply(rator, val);
+	break;
+      }
+    case DEL:
+      {
+	Cell* rand = cont->r;
+	POPCONT;
+	val = apply(val, rand);
+	break;
+      }
+    case FINAL:
+      return;
+    default:
+      errexit("[BUG] run: invalid continuation type %d\n", cont->t);
+    }
+  }
+}
+
+int main(int argc, char *argv[]) {
+  Cell* root;
+  clock_t start;
+  char *prog_file = NULL;
+  int i;
+  int print_stats = 1;
+    
+  for (i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-g") == 0)
+      gc_notify = 1;
+    else if (strcmp(argv[i], "-s") == 0)
+      print_stats = 1;
+    else if (strcmp(argv[i], "-u") == 0)
+      setbuf(stdout, NULL);
+    else
+      prog_file = argv[i];
+  }
+
+  storage_init(INITIAL_HEAP_SIZE);
+
+  root = load_program(prog_file);
+
+  start = clock();
+  run(root);
+
+  if (print_stats) {
+    double evaltime = (clock() - start) / (double)CLOCKS_PER_SEC;
+
+    printf("  total eval time --- %5.2f sec.\n", evaltime - total_gc_time);
+    printf("  total gc time   --- %5.2f sec.\n", total_gc_time);
+  }
+  return 0;
+}
